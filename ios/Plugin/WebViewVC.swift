960eb827387e5c78617473d7b9fb35f53e4a42b6
Sandip M <sandip@rewaatech.com>
2023-11-07 15:44:07 +0530

    Fixed empty space issue on image

--- ios/Plugin/WebViewVC.swift
+++ ios/Plugin/WebViewVC.swift
@@ -23,31 +23,20 @@ class WebViewVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        scrollView = UIScrollView(frame: CGRect(x: 0, y: 0, width: self.view.frame.width, height: self.view.frame.height))
-        scrollView.contentSize = CGSize(width: self.view.frame.width, height: self.view.frame.height)
-        
-        self.view.addSubview(scrollView)
-        
         if UIDevice.current.userInterfaceIdiom == .pad {
             self.view.backgroundColor = .none
         } else {
             self.view.backgroundColor = .white
         }
         
-        scrollView.translatesAutoresizingMaskIntoConstraints = false
-        scrollView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor).isActive = true
-        scrollView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor).isActive = true
-        scrollView.topAnchor.constraint(equalTo: self.view.topAnchor).isActive = true
-        scrollView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor).isActive = true
+        self.htmlView.frame = CGRect(x: 0, y: 0, width: 600, height: 0)
         
-        self.htmlView.frame = CGRect(x: 0, y: 0, width: 300, height: scrollView.contentSize.height)
+        self.view.addSubview(htmlView)
         
-        scrollView.addSubview(htmlView)
-        
-        htmlView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor).isActive = true
-        htmlView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor).isActive = true
-        htmlView.topAnchor.constraint(equalTo: scrollView.topAnchor).isActive = true
-        htmlView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor).isActive = true
+        htmlView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor).isActive = true
+        htmlView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor).isActive = true
+        htmlView.topAnchor.constraint(equalTo: self.view.topAnchor).isActive = true
+        htmlView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor).isActive = true
         
         self.htmlView.backgroundColor = .yellow
         htmlView.html = htmlContentString
@@ -79,48 +68,28 @@ extension WebViewVC: PSHTMLViewDelegate {
     
     func didFinishLoad() {
         
-        scrollView.contentSize = CGSize(width: scrollView.contentSize.width, height: htmlView.webViewHeightConstraint.constant)
-        self.htmlView.frame = CGRect(x: 0, y: 0, width: scrollView.contentSize.width, height: scrollView.contentSize.height)
-        
-        let bottomOffset = CGPoint(x: 0, y: scrollView.contentSize.height - scrollView.bounds.height + scrollView.contentInset.bottom)
-        scrollView.setContentOffset(bottomOffset, animated: true)
+        self.htmlView.frame = CGRect(x: 0, y: 0, width: self.view.frame.width - 100, height: htmlView.webViewHeightConstraint.constant)
         
         DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
-            // your code here
             let image = self.htmlView.image()
-            let resizeImage = image?.resizeImageTest(image: image!, targetSize: CGSize(width: 600, height: 2400))
-            let imageData:Data = (resizeImage?.pngData())!
+            let resizeImage = self.imageWithImage(sourceImage: image!, scaledToWidth: 600)
+            let imageData:Data = (resizeImage.pngData())!
             let imageBase64String = imageData.base64EncodedString()
             self.delegatePassHTMLContent?.passHTMLContent(base64: imageBase64String)
         }
     }
-}
-
-//MARK: resize Image
-extension UIImage {
-    func resizeImageTest(image: UIImage, targetSize: CGSize) -> UIImage {
-        let size = image.size
+    
+    func imageWithImage (sourceImage:UIImage, scaledToWidth: CGFloat) -> UIImage {
+        let oldWidth = sourceImage.size.width
+        let scaleFactor = scaledToWidth / oldWidth
         
-        let widthRatio  = targetSize.width  / image.size.width
-        let heightRatio = targetSize.height / image.size.height
+        let newHeight = sourceImage.size.height * scaleFactor
+        let newWidth = oldWidth * scaleFactor
         
-        // Figure out what our orientation is, and use that to form the rectangle
-        var newSize: CGSize
-        if(widthRatio > heightRatio) {
-            newSize = CGSizeMake(size.width * heightRatio, size.height * heightRatio)
-        } else {
-            newSize = CGSizeMake(size.width * widthRatio,  size.height * widthRatio)
-        }
-        
-        // This is the rect that we've calculated out and this is what is actually used below
-        let rect = CGRectMake(0, 0, /*newSize.width*/targetSize.width, newSize.height)
-        
-        // Actually do the resizing to the rect using the ImageContext stuff
-        UIGraphicsBeginImageContextWithOptions(newSize, false, 1.0)
-        image.draw(in: rect)
+        UIGraphicsBeginImageContext(CGSize(width:newWidth, height:newHeight))
+        sourceImage.draw(in: CGRect(x:0, y:0, width:newWidth, height:newHeight))
         let newImage = UIGraphicsGetImageFromCurrentImageContext()
         UIGraphicsEndImageContext()
-        
         return newImage!
     }
 }
